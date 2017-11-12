d8c637b7f4feb0bec66fdcf72cc54c15cf0a23f2
Dirk Hohndel <dirk@hohndel.org>
2017-11-12 08:40:58 -0800

    different approach to caching
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/mac/before_install.sh
+++ scripts/mac/before_install.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-rvm get stable --auto-dotfiles
-rvm install 2.3.0
+#
+# maybe I don't need to do this if I use brew update? The documentation
+# is not all that great
+#
+#rvm get stable --auto-dotfiles
+#rvm install 2.3.0
 
 # install a couple more homebrew components
-brew install hidapi libusb libxml2 libxslt libzip openssl pka-config libgit2
+if [ ! -d ${TRAVIS_BUILD_DIR}/Homebrew ] ; then
+	# trying to get Homebrew taken care of and hopefully get cached
+	brew update
+	brew install hidapi libusb libxml2 libxslt libzip openssl pkg-config libgit2
+	mkdir -p ${TRAVIS_BUILD_DIR}/Homebrew
+	tar cJf ${TRAVIS_BUILD_DIR}/Homebrew/local.tar.xz /usr/local
+else
+	echo "Homebrew is in cache"
+	find /usr/local
+	sudo tar xJfC ${TRAVIS_BUILD_DIR}/Homebrew/local.tar.xz /usr/local
+	find /usr/local
+fi
 
 # prep things so we can build for Mac
 # we have a custom built Qt some gives us just what we need, including QtWebKit
